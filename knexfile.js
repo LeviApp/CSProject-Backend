172c5eb1a8c3ccf1bde791b6302d0fbb2d61767d
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-03-31 12:09:52 -0600

    added a pool property to knex

--- knexfile.js
+++ knexfile.js
@@ -23,6 +23,16 @@ module.exports = {
   production: {
     client: "pg",
     connection: CON,
+    "pool": {
+      "min": 2,
+      "max": 10,
+      "createTimeoutMillis": 3000,
+      "acquireTimeoutMillis": 30000,
+      "idleTimeoutMillis": 30000,
+      "reapIntervalMillis": 1000,
+      "createRetryIntervalMillis": 100,
+      "propagateCreateError": false // <- default is true, set to false
+    },
     migrations: {
       tableName: "map",
       directory: "./migrations"
